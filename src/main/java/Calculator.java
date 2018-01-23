b47b843078ad4bc3c5f52ee57db724d58092685f
Urszula Jajtner <urszula.jajtner@gmail.com>
2018-01-23 20:16:39 +0100

    "Calculator"

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public float plus(float a, float b){
+        return a+b;
+    }
+
+    public float minus(float a, float b){
+        return a-b;
+    }
+
+    public float times(float a, float b){
+        return a*b;
+    }
+
+    public float by(float a, float b){
+        return a/b;
+    }
+}
